76b9a0590fb40f67ca13edca4045d48b34388d96
bpptk <bpptk@bpptk-PC>
2017-05-09 01:27:58 -0700

    add DAO

---
+++ src/bukutelepon/dao/DaoModelBukuTelepon.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bukutelepon.dao;
+
+import bukutelepon.model.ModelBukuTelepon;
+import java.util.List;
+
+/**
+ *
+ * @author bpptk
+ */
+public class DaoModelBukuTelepon implements ImplementModelBukuTelepon{
+
+    @Override
+    public void insert(ModelBukuTelepon b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(ModelBukuTelepon b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<ModelBukuTelepon> getALL() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<ModelBukuTelepon> getCariNama(String nama) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
